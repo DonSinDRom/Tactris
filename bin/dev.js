400941f4aa4229c0bf4f45a928b695410d7305cf
DonSinDRom <DonSinDRom@gmail.com>
2015-07-26 21:49:03 +0300

    Recovered

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-//#!/usr/bin/env node
+#!/usr/bin/env node
 
 'use strict';
 
@@ -11,7 +11,7 @@ var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 var chalk = require('chalk');
 
-var b = browserify(path.resolve('../src/index.js'), watchify.args);
+var b = browserify(path.resolve('./src/index.js'), watchify.args);
 var w = watchify(b);
 
 var bytes, time;
@@ -20,7 +20,7 @@ w.on('time', function (t) { time = t });
 
 var update = function(bundle) {
     var didError = false;
-    var writeStream = fs.createWriteStream(path.resolve('../public/bundle.js'));
+    var writeStream = fs.createWriteStream(path.resolve('./public/bundle.js'));
 
     bundle.on('error', function (err) {
         console.error(String(chalk.red(err)));
@@ -36,7 +36,7 @@ var update = function(bundle) {
 
     writeStream.on('close', function () {
         if (!didError) {
-            console.error(chalk.cyan(bytes) + chalk.grey(' bytes written to ') + chalk.cyan(path.resolve('../public/bundle.js'))
+            console.error(chalk.cyan(bytes) + chalk.grey(' bytes written to ') + chalk.cyan(path.resolve('./public/bundle.js'))
                 + ' (' + (time / 1000).toFixed(2) + ' seconds)'
             );
         }
@@ -49,10 +49,10 @@ w.on('update', function (ids) {
     update(w.bundle());
 });
 
-var serve = serveStatic(path.normalize('../public/'));
+var serve = serveStatic(path.normalize('./public/'));
 
 var server = http.createServer(function(req, res){
   serve(req, res, finalhandler(req, res))
 });
 
-server.listen(1618, function() {console.log(chalk.grey('serving ') + chalk.blue(path.resolve('../public/')) + chalk.grey(' on port ') + chalk.blue('1618'));});
+server.listen(1618, function() {console.log(chalk.grey('serving ') + chalk.blue(path.resolve('./public/')) + chalk.grey(' on port ') + chalk.blue('1618'));});
