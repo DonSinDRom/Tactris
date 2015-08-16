a50ee4a9e46a16cf6b77ee841f6bfceb48c1fddc
DonSinDRom <DonSinDRom@gmail.com>
2015-08-16 14:50:34 +0300

    Added polyfill for Array.some

--- src/index.js
+++ src/index.js
@@ -84,6 +84,31 @@ if (!Array.prototype.every) {
 		return true;
 	};
 }
+if (!Array.prototype.some) {
+	Array.prototype.some = function (fun /*, thisArg*/ ) {
+		'use strict';
+
+		if (this == null) {
+			throw new TypeError('Array.prototype.some called on null or undefined');
+		}
+
+		if (typeof fun !== 'function') {
+			throw new TypeError();
+		}
+
+		var t = Object(this);
+		var len = t.length >>> 0;
+
+		var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+		for (var i = 0; i < len; i++) {
+			if (i in t && fun.call(thisArg, t[i], i, t)) {
+				return true;
+			}
+		}
+
+		return false;
+	};
+}
 if (!Array.prototype.filter) {
 	Array.prototype.filter = function (fun /*, thisArg*/ ) {
 		'use strict';
