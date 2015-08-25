2f99195185f510defd51555fb3a7d061b738a9f4
DonSinDRom <DonSinDRom@gmail.com>
2015-08-26 02:53:25 +0300

    Add Modal window

---
+++ src/Modal.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var famous = require('famous');
+var Consts = require('./Consts.js');
+
+var Node = famous.core.Node;
+var Position = famous.components.Position;
+var DOMElement = famous.domRenderables.DOMElement;
+
+function Modal() {
+	Node.call(this);
+
+	this
+		.setPosition(0, 0, 0);
+
+	this.domElement = new DOMElement(this, {
+		tagName: 'center',
+		classes: ['Modal'],
+		properties: {
+			color: '#fff',
+			fontSize: '24px',
+			padding: '1rem',
+			backgroundColor: 'rgba(0,0,0,.6)'
+		},
+		content: 'Modal'
+	});
+
+	this.hide = function hide() {
+		this.position.setY(-Consts.HEIGHT, {
+			duration: Consts.MODAL_DURATION,
+			curve: Consts.MODAL_CURVE
+		});
+	};
+
+	this.show = function show() {
+		this.domElement.setContent(`<h1>Game Over!<br>Click to start new game</h1>`);
+		this.position.setY(0, {
+			duration: Consts.MODAL_DURATION,
+			curve: Consts.MODAL_CURVE
+		});
+	};
+
+	this.position = new Position(this);
+	this.position.setY(-Consts.HEIGHT);
+
+	this.addUIEvent('click');
+}
+
+Modal.prototype = Object.create(Node.prototype);
+Modal.prototype.constructor = Modal;
+
+Modal.prototype.onReceive = function onReceive(type, ev) {
+	switch (type) {
+	case 'click':
+			this.hide();
+			this._parent.gameStart();
+			break;
+	default:
+			return false;
+	}
+};/*jshint +W074 */
+
+module.exports = Modal;
